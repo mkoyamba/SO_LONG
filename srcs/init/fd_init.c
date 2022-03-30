96aa0d66e7090cba3c2d0e8932e77b7c10823ab5
Maxime Koyamba <mkoyamba@fu-r6-p4.s19.be>
2022-03-30 11:57:39 +0200

    patch

--- srcs/init/fd_init.c
+++ srcs/init/fd_init.c
@@ -6,7 +6,7 @@
 /*   By: mkoyamba <mkoyamba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 15:54:53 by mkoyamba          #+#    #+#             */
-/*   Updated: 2022/03/17 21:08:16 by mkoyamba         ###   ########.fr       */
+/*   Updated: 2022/03/30 11:52:39 by mkoyamba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,27 @@ static void	set_player(t_data *data)
 	}
 }
 
+static int	ber_file(char *str)
+{
+	size_t	x;
+
+	x = ft_strlen(str);
+	if (str[x - 4] != '.' || str[x - 3] != 'b' || str[x - 2] != 'e'
+		|| str[x - 1] != 'r')
+		return (0);
+	return (1);
+}
+
 void	fd_init(t_data *data, char **av, int ac)
 {
-	if (ac != 2)
+	if (ac != 2 || ft_strlen(av[1]) < 5)
 	{
 		ft_printf("Error\n./so_long [map_path]\n");
+		exit(0);
+	}
+	if (!ber_file(av[1]))
+	{
+		ft_printf("Error\n.La map doit être au format .ber\n");
 		exit(0);
 	}
 	data->fd = open(av[1], O_RDONLY);
